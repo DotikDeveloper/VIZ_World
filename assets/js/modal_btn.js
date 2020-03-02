b10d47e2344a9987247fb861b58d0c61aa98ab75
Dotik <36793535+DotikDeveloper@users.noreply.github.com>
2020-03-02 19:49:02 +0400

    Add files via upload
    The final set of files is loaded. Additionally, you need to enable page editing here

---
+++ assets/js/modal_btn.js
@@ -0,0 +1,7 @@
+$('.btnNext').click(function() {
+    $('.nav-tabs .active').parent().next('li').find('a').trigger('click');
+});
+
+$('.btnPrevious').click(function() {
+    $('.nav-tabs .active').parent().prev('li').find('a').trigger('click');
+});
